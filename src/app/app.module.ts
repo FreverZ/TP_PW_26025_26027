645ea789d44c139b76c7c4aaac75a84be9f40ca0
Carneiro <79153593+Carneirooo@users.noreply.github.com>
2024-06-09 16:37:10 +0100

    mais uma do carneiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routes'; // Certifique-se de usar o nome correto aqui
 import { SharedModule } from './pages/shared/shared.module';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { Museu1Component } from './pages/museu1/museu1.component';
+import { Museu2Component } from './pages/museu2/museu2.component';
+import { Museu3Component } from './pages/museu3/museu3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent,
+    Museu1Component,
+    Museu2Component,
+    Museu3Component
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { Museu1Component } from './pages/museu1/museu1.component';
+import { Museu2Component } from './pages/museu2/museu2.component';
+import { Museu3Component } from './pages/museu3/museu3.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'museu1', component: Museu1Component },
+  { path: 'museu2', component: Museu2Component },
+  { path: 'museu3', component: Museu3Component },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
