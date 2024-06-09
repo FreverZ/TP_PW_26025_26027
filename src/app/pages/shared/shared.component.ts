ec3e71288cf3224630381944b3e1292bf64f16d5
Carneiro <79153593+Carneirooo@users.noreply.github.com>
2024-06-09 16:43:32 +0100

    acho que já dá

--- src/app/pages/shared/shared.component.ts
+++ src/app/pages/shared/shared.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'solid-button',
-  templateUrl: './solid-button.component.html',
-  styleUrls: ['./solid-button.component.css']
+  templateUrl: './solid-button/solid-button.component.html', // Caminho corrigido
+  styleUrls: ['./solid-button/solid-button.component.css'] // Caminho corrigido
 })
 export class SolidButtonComponent {
   @Input() button: string = '';
