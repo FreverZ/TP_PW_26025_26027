18c1ad4d572a02d3e4b71b2239da54f9f6795d46
Carneiro <79153593+Carneirooo@users.noreply.github.com>
2024-06-10 18:56:36 +0100

    1520

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -5,4 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent { }
+export class RegisterComponent {
+}
