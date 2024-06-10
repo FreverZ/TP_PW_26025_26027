2e0c55f636472e6bf3832074e25ad3950647194e
Carneiro <79153593+Carneirooo@users.noreply.github.com>
2024-06-10 19:20:01 +0100

    145

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Importando componentes
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ComponentsModule } from './components/components.module';
+import { Register1Component } from './components/register1/register1.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { Museu1Component } from './pages/museu1/museu1.component';
+import { Museu2Component } from './pages/museu2/museu2.component';
+import { Museu3Component } from './pages/museu3/museu3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    LoginComponent
+    Register1Component,
+    GalleryComponent,
+    PricingComponent,
+    ReviewsComponent,
+    Museu1Component,
+    Museu2Component,
+    Museu3Component
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: '', component: LandingPageComponent },
-      { path: 'login', component: LoginComponent }
-    ]),
-    CommonModule,
-    ComponentsModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
